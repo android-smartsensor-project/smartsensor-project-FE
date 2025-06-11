76a2ad24093130a87fe4d74224ea161983275ac2
tree345 <syyu0405dowe@gmail.com>
2025-06-11 17:54:37 +0900

    메인페이지 변경과 회원가입2 페이지 변경

--- app/src/main/java/com/example/myapplication/CustomMarkerView.java
+++ app/src/main/java/com/example/myapplication/CustomMarkerView.java
@@ -11,30 +11,35 @@ import com.github.mikephil.charting.utils.MPPointF;
 public class CustomMarkerView extends MarkerView {
 
     private TextView tvContent;
-    private String activityLabel = ""; // 운동량 라벨을 저장할 변수 추가
+    private String activityLabel = "";
 
     public CustomMarkerView(Context context, int layoutResource) {
         super(context, layoutResource);
         tvContent = findViewById(R.id.tvContent);
     }
 
-    // 외부에서 운동량 라벨을 설정할 수 있는 메소드 추가
     public void setActivityLabel(String label) {
         this.activityLabel = label;
     }
 
     @Override
     public void refreshContent(Entry e, Highlight highlight) {
-        int hour = (int) e.getX(); // 시간 (0~23)
-        int activityValue = (int) e.getY(); // 운동량 값 (정수로 형변환)
+        int hour = (int) e.getX();
+        int activityValue = (int) e.getY();
 
-        // 시간대 정보 문자열
         String timeRange = hour + "시 ~ " + (hour + 1) + "시";
 
-        // 운동량 정보 문자열 생성
-        String activityInfo = activityValue + " " + activityLabel.replace("수", "").replace("시간", " 활동").replace("칼로리", " 소모"); // 라벨에 따라 단위를 붙여줌
+        String activityInfo;
+        if ("걸음 수".equals(activityLabel)) {
+            activityInfo = activityValue + " km/s";
+        } else if ("활동 시간".equals(activityLabel)) {
+            activityInfo = activityValue + " 분 활동";
+        } else if ("활동 칼로리".equals(activityLabel)) {
+            activityInfo = activityValue + " 칼로리 소모";
+        } else {
+            activityInfo = activityLabel + ": " + activityValue;
+        }
 
-        // 최종적으로 MarkerView에 표시할 텍스트 설정 (시간대와 운동량 정보를 줄 바꿈으로 연결)
         tvContent.setText(timeRange + "\n" + activityInfo);
 
         super.refreshContent(e, highlight);
@@ -42,7 +47,6 @@ public class CustomMarkerView extends MarkerView {
 
     @Override
     public MPPointF getOffset() {
-        // MarkerView가 막대 위 중앙에 오도록 오프셋 설정
         return MPPointF.getInstance(-(getWidth() / 2), -getHeight());
     }
 }
